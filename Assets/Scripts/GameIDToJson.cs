28454b7e52bc10863ff56ac3257a485cefb2433e
Manh <manhdeptraivodich@gmail.com>
2024-10-29 14:22:38 +0700

    Update Path Android vs IOS

--- Assets/Scripts/GameIDToJson.cs
+++ Assets/Scripts/GameIDToJson.cs
@@ -5,18 +5,28 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+public enum TypeApp
+{
+    Android,
+    IOS
+}
 public class GameIDToJson : MonoBehaviour
 {
 #if UNITY_EDITOR
+    [SerializeField] private TypeApp _typeApp;
+
     [FolderPath, Header("Path to panels")]
-    public string path;
+    public string pathAndroid;
+
+    [FolderPath, Header("Path to panels")]
+    public string pathIOS;
 #endif
     [Button]
     public void SaveGameIDToJsonFile(GameId gameId)
     {
         string json = JsonConvert.SerializeObject(gameId, Formatting.Indented);
 
-        string filePath = Path.Combine(path, gameId.packageName + ".json");
+        string filePath = Path.Combine(_typeApp == TypeApp.Android ? pathAndroid : pathIOS, gameId.packageName + ".json");
 
         File.WriteAllText(filePath, json);
 
