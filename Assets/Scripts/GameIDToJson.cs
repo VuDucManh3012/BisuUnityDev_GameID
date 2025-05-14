ab1eead45199de9fd3b6ce5bc50a7c49f953c29d
Manh <manhdeptraivodich@gmail.com>
2025-05-14 14:48:24 +0700

    Add Clean String

--- Assets/Scripts/GameIDToJson.cs
+++ Assets/Scripts/GameIDToJson.cs
@@ -24,6 +24,8 @@ public class GameIDToJson : MonoBehaviour
     [Button]
     public void SaveGameIDToJsonFile(GameId gameId)
     {
+        CleanString(gameId);
+
         string json = JsonConvert.SerializeObject(gameId, Formatting.Indented);
 
         string filePath = Path.Combine(_typeApp == TypeApp.Android ? pathAndroid : pathIOS, gameId.packageName + ".json");
@@ -31,5 +33,28 @@ public class GameIDToJson : MonoBehaviour
         File.WriteAllText(filePath, json);
 
         Debug.Log("Init Json Succeed");
+    }
+
+    void CleanString(GameId gameId)
+    {
+        gameId.packageName = RemoveEmptyEntries(gameId.packageName);
+        gameId.adjustID = RemoveEmptyEntries(gameId.adjustID);
+
+        gameId.admobId = RemoveEmptyEntries(gameId.admobId);
+
+        gameId.bannerId = RemoveEmptyEntries(gameId.bannerId);
+        gameId.interId = RemoveEmptyEntries(gameId.interId);
+        gameId.rewardId = RemoveEmptyEntries(gameId.rewardId);
+        gameId.aoaId = RemoveEmptyEntries(gameId.aoaId);
+        gameId.maxDevKey = RemoveEmptyEntries(gameId.maxDevKey);
+
+        for (int i = 0; i < gameId.listNativeId.Count; i++)
+        {
+            gameId.listNativeId[i] = RemoveEmptyEntries(gameId.listNativeId[i]);
+        }
+    }
+    string RemoveEmptyEntries(string input)
+    {
+        return string.Join(" ", input.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries));
     }
 }
